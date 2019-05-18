28e56bcf7235ea1fca92cd2193461e5c99fdd222
timoxin.artur@gmail.com <timoxin.artur@gmail.com>
2019-05-18 22:58:10 +0300

    fix style main page, add cyrilic font

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,9 +1,5 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
-import img1 from "../../assets/img/carousel/1920/1_1920.jpg";
-import img2 from "../../assets/img/carousel/1920/2_1920.jpg";
-import img3 from "../../assets/img/carousel/1920/3_1920.jpg";
-import img4 from "../../assets/img/carousel/1920/4_1920.jpg";
 import logoTitle from "../../assets/img/logo-and-title-white-300.png";
 
 export default function MainPage() {
@@ -12,16 +8,16 @@ export default function MainPage() {
       <img src={logoTitle} alt="" id="logoAndTitle" />
       <Carousel controls={false} indicators={false} interval={3000} pauseOnHover={false}>
         <Carousel.Item>
-          <img className="d-block w-100" src={img1} alt="First slide" />
+          <div style={{ backgroundImage: "url(http://localhost:3001/main-page/1_1920.jpg)" }} />
         </Carousel.Item>
         <Carousel.Item>
-          <img className="d-block w-100" src={img2} alt="Third slide" />
+          <div style={{ backgroundImage: "url(http://localhost:3001/main-page/2_1920.jpg)" }} />
         </Carousel.Item>
         <Carousel.Item>
-          <img className="d-block w-100" src={img3} alt="Third slide" />
+          <div style={{ backgroundImage: "url(http://localhost:3001/main-page/3_1920.jpg)" }} />
         </Carousel.Item>
         <Carousel.Item>
-          <img className="d-block w-100" src={img4} alt="Third slide" />
+          <div style={{ backgroundImage: "url(http://localhost:3001/main-page/4_1920.jpg)" }} />
         </Carousel.Item>
       </Carousel>
     </div>
